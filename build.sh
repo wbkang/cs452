8ba65a02ab5e151babc2da4739209138f9120e4d
pbakhila <1944571+pavel-bakhilau@users.noreply.github.com>
2011-09-21 18:05:05 -0400

    fixed build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+set -x
 set -e
 
 LIBS=kernel
 
+mkdir -p lib; 
+
 for i in $LIBS; do
-	(cd $i;  make; mkdir -p lib; cp $i.a ../lib/lib$i.a);
+	(cd $i;  make; cp $i.a ../lib/lib$i.a);
 done
 
 (cd main; make );
 
 cp main/main.elf /u/cs452/tftp/ARM/space.elf
+chmod 770 /u/cs452/tftp/ARM/space.elf
 
 echo BUILT AND DEPLOYED
