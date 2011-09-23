cf40d413e69c957f75fadf863a7433e9539340ea
wbkang <wbkang@localhost>
2011-09-23 18:35:32 -0400

    Refactoring... Added assembly.h, kernel, and test. Relocated interrupt.S

--- main/main.c
+++ main/main.c
@@ -5,46 +5,17 @@
 #include <task.h>
 #include <memory.h>
 
-// asm
-void asm_handle_swi();
 
-void handle_swi(register_set reg, int service_no) {
-	bwprintf(COM2, "Dumping registers...\n");
-	for (int i = 0; i < 15; i++)
-	{
-		bwprintf(COM2, "r%d: %x\n", i, reg->registers[i]);
-	}
-}
 
-static void install_interrupt_handlers() {
-	INSTALL_INTERRUPT_HANDLER(0x8, asm_handle_swi);
-}
+
+
 
 int main(int argc, char* argv[]) {
-	uart_fifo(COM2, OFF);
-	
-	install_interrupt_handlers();
+	raw_init();
 
-	struct _tag_pages _p;
-	pages p = &_p;
-	memptr node_table[8];
-	pages_init(p, 8, (uint) node_table, 0x300000);
-	memptr test[8];
-	int i;
-	for (i = 7; i >= 0; --i) {
-		test[i] = pages_get(p);
-		bwprintf(COM2, "page %d: %x\n", i + 1, (uint) test[i]);
-	}
-	bwprintf(COM2, "head after remove 5: %x\n", (uint) p->head);
-	for (i = 7; i >= 0; --i) {
-		pages_put(p, test[7 - i]);
-		bwprintf(COM2, "head %d: %x\n", i + 1, (uint) p->head);
-	}
+	test_run();
 
-
-	ASM(
-			"swi 0" "\n\t"
-	);
+	kernel_init();
 
 	return 0;
 }
---
+++ main/test.c
@@ -0,0 +1,32 @@
+#include <test.h>
+#include <util.h>
+#include <hardware.h>
+
+static void test_pages()
+{
+	struct _tag_pages _p;
+	pages p = &_p;
+	memptr node_table[8];
+	pages_init(p, 8, (uint) node_table, 0x300000);
+	memptr test[8];
+	int i;
+	for (i = 7; i >= 0; --i) {
+		test[i] = pages_get(p);
+		bwprintf(COM2, "page %d: %x\n", i + 1, (uint) test[i]);
+	}
+	bwprintf(COM2, "head after remove 5: %x\n", (uint) p->head);
+
+	ASSERT(p->head == 0, "there should be no more pages to allocate.");
+
+	for (i = 7; i >= 0; --i) {
+		pages_put(p, test[7 - i]);
+		bwprintf(COM2, "head %d: %x\n", i + 1, (uint) p->head);
+	}
+}
+
+void test_run()
+{
+	bwprintf(COM2, ">>>>>>>>>>>>>>>>>>>>TEST START\n");
+	test_pages();
+	bwprintf(COM2, ">>>>>>>>>>>>>>>>>>>>TEST END\n");
+}
