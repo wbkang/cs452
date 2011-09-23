565602f6835c1272789e700870807eda75369377
pbakhila <pbakhila@localhost>
2011-09-23 10:42:34 -0400

    fixed build script yet again

--- build.sh
+++ build.sh
@@ -11,7 +11,10 @@ done
 
 (cd main; make );
 
-cp main/main.elf /u/cs452/tftp/ARM/space.elf
-chmod 776 /u/cs452/tftp/ARM/s.elf
+ELF=/u/cs452/tftp/ARM/s.elf
+
+cp main/main.elf $ELF
+chgrp cs452_sf $ELF
+chmod 776 $ELF
 
 echo BUILT AND DEPLOYED
