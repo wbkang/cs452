1213d550dd72c03e9382e4120540fccd7064545d
pbakhila <1944571+pavel-bakhilau@users.noreply.github.com>
2011-09-23 18:45:01 -0400

    improved paging test and moved to memory.c

--- include/memory.h
+++ include/memory.h
@@ -7,11 +7,11 @@
 
 typedef struct _tag_pages {
 		memptr head;
-		uint node_table;
+		uint first_node;
 		uint first_page;
 }*pages;
 
-void pages_init(pages p, uint size, uint node_table, uint first_page);
+void pages_init(pages p, uint size, uint first_node, uint first_page);
 memptr pages_nextnode(memptr node);
 uint pages_nodeindex(pages p, memptr node);
 uint pages_pageindex(pages p, memptr page);
@@ -19,3 +19,4 @@ memptr pages_node2page(pages p, memptr node);
 memptr pages_page2node(pages p, memptr page);
 memptr pages_get(pages p);
 void pages_put(pages p, memptr page);
+void pages_test();
--- kernel/memory.c
+++ kernel/memory.c
@@ -1,13 +1,16 @@
 #include <memory.h>
+#include <util.h>
+#include <rawio.h>
+#include <ts7200.h>
 
-void pages_init(pages p, uint size, uint node_table, uint first_page) {
-	p->head = (memptr) node_table;
-	p->node_table = node_table;
+void pages_init(pages p, uint size, uint first_node, uint first_page) {
+	p->head = (memptr) first_node;
+	p->first_node = first_node;
 	p->first_page = first_page;
 	// initialize nodes
-	uint addr = node_table + 4 * (size - 1); // bottom
+	uint addr = first_node + 4 * (size - 1); // bottom
 	MEM(addr) = NULL;
-	for (; addr > node_table; addr -= 4) {
+	for (; addr > first_node; addr -= 4) {
 		MEM(addr - 4) = addr;
 	}
 }
@@ -17,7 +20,7 @@ memptr pages_nextnode(memptr node) {
 }
 
 uint pages_nodeindex(pages p, memptr node) {
-	return ((uint) node) - p->node_table;
+	return ((uint) node) - p->first_node;
 }
 
 uint pages_pageindex(pages p, memptr page) {
@@ -29,12 +32,12 @@ memptr pages_node2page(pages p, memptr node) {
 }
 
 memptr pages_page2node(pages p, memptr page) {
-	return (memptr) (p->node_table + (pages_pageindex(p, page) >> PAGE_BSIZE));
+	return (memptr) (p->first_node + (pages_pageindex(p, page) >> PAGE_BSIZE));
 }
 
-memptr pages_get(pages p) { // allocate a page
+memptr pages_get(pages p) {
 	memptr head = p->head;
-	if (head == NULL) return NULL; // no more pages to give
+	if (head == NULL) return NULL;
 	p->head = pages_nextnode(head);
 	return pages_node2page(p, head);
 }
@@ -43,4 +46,23 @@ void pages_put(pages p, memptr page) {
 	memptr node = pages_page2node(p, page);
 	*node = (uint) p->head;
 	p->head = node;
+}
+
+void pages_test() {
+	int size = 1024;
+	struct _tag_pages _p;
+	pages p = &_p;
+	memptr node_table[size];
+	pages_init(p, size, (uint) node_table, 0x300000);
+	memptr test[size];
+	int i;
+	for (i = size - 1; i >= 0; --i) {
+		test[i] = pages_get(p);
+		bwprintf(COM2, "page %d: %x\n", i + 1, (uint) test[i]);
+	}
+	bwprintf(COM2, "head after remove %d: %x\n", size, (uint) p->head);
+	for (i = size - 1; i >= 0; --i) {
+		pages_put(p, test[size - 1 - i]);
+		bwprintf(COM2, "head %d: %x\n", i + 1, (uint) p->head);
+	}
 }
