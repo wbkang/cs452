29ed8c55258753c02969740636879a83327f0573
pbakhila <pbakhila@localhost>
2011-09-29 11:54:47 -0400

    usermode works

--- kernel/util.c
+++ kernel/util.c
@@ -12,6 +12,6 @@ void errormsg(char* msg) {
 	bwprintf(COM2, msg);
 }
 
-void dump_registers(int r0, int r1, int r2, int r3, int r4) {
-	TRACE("dump_registers:\n\tr0: %x\n\tr1: %x\n\tr2: %x\n\tr3: %x\n\tr4: %x\n", r0, r1, r2, r3, r4);
+void dump_registers(int r0, int r1, int r2, int r3) {
+	TRACE("dump_registers:\n\tr0: %x\n\tr1: %x\n\tr2: %x\n\tr3: %x\n", r0, r1, r2, r3);
 }
