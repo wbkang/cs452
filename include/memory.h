a9c7b1ab77d324dd84b6581f2c393f02e4a047b5
pbakhila <pbakhila@localhost>
2011-09-30 17:04:43 -0400

    refactoring

--- include/memory.h
+++ include/memory.h
@@ -11,6 +11,10 @@
 // this is calculated in compile time.
 #define TASK_LIST_SIZE ((USER_MEM_END - USER_MEM_START) / 65536)
 
+#define BYTES2WORDS(x) ((x) >> 2)
+
+#define NEXTHIGHESTWORD(x) BYTES2WORDS((x) + 3)
+
 void mem_init();
 
 void mem_reset();
--- kernel/memory.c
+++ kernel/memory.c
@@ -5,7 +5,6 @@
 #include <syscall.h>
 
 extern uint _KERNEL_MEM_START;
-
 static memptr kernel_heap;
 static stack *umpages;
 
@@ -20,24 +19,20 @@ void mem_init() {
 }
 
 void mem_reset() {
-	kernel_heap = &_KERNEL_MEM_START;
+	mem_init();
 }
 
-void* kmalloc(uint size) { // requires size in bytes
+void* kmalloc(uint size) {
 	memptr rv = kernel_heap;
-	kernel_heap += (size + 3) >> 2; // round up to nearest word
+	kernel_heap += NEXTHIGHESTWORD(size);
 	ASSERT((int) kernel_heap < USER_MEM_START, "kernel heap overflow");
 	return rv;
 }
 
-void* umalloc(uint size) { // requires size in bytes
+void* umalloc(uint size) {
 	volatile task_descriptor *td = scheduler_running();
-	uint rounded_up = (size + 3) >> 2; // round up to nearest word
 	memptr rv = td->heap;
-	td->heap += rounded_up;
-	/*TRACE("sp: %x, heap: %x, size:%d, roundedup:%d",
-	 (uint) td->registers.r[REG_SP], (uint) td->heap, size,
-	 rounded_up);*/
+	td->heap += NEXTHIGHESTWORD(size);
 	ASSERT(((uint)td->registers.r[REG_SP]) > (uint)td->heap,
 			"user task ran out of memory");
 	return rv;
@@ -68,7 +63,7 @@ void* qmalloc(uint size) { // requires size in bytes
 void allocate_user_memory(task_descriptor *td) {
 	td->heap_base = (memptr) stack_pop(umpages);
 	td->heap = td->heap_base;
-	td->registers.r[REG_SP] = (int) td->heap + (STACK_SIZE >> 2);
+	td->registers.r[REG_SP] = ((int) td->heap) + BYTES2WORDS(STACK_SIZE);
 }
 
 void free_user_memory(task_descriptor *td) {
