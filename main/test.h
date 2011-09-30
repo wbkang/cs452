8800512fd7db7a8252128bbeaf452e3a5ea79ee2
Woongbin Kang <wbk@outlook.com>
2011-09-29 23:19:13 -0400

    Added kernel test files.

--- main/test.h
+++ main/test.h
@@ -1,3 +1,19 @@
 #pragma once
 
+#include <hardware.h>
+#include <rawio.h>
+#include <util.h>
+
+#define TEST_START(name) TRACE("\t%s ... ", name)
+
+#define TEST_END(name) TRACE("looks good\n")
+
+#define EXPECT(expected, got) { \
+	if (got != expected) { \
+		bwprintf(COM2, "ERROR!!!\nExpected %d but got %d (%x)", expected, got, got); \
+		die(); \
+	} \
+}
+
+
 void test_run();
