56870625507c0dc5ec8c3459df2c644f8bbbb67c
wbkang <wbkang@localhost>
2011-09-30 17:20:32 -0400

    Kernel 1 for submission.

--- include/util.h
+++ include/util.h
@@ -35,9 +35,9 @@ typedef char *va_list;
 // turn mask bits on/off in word based on flag (improve with orr/bic?)
 #define BIT_TOGGLE(word, mask, flag) ((word) ^= (-(flag) ^ (word)) & (mask))
 ///////////// DEBUG
-#define ASSERT_ENABLED 1
-#define TRACE_ENABLED 1
-#define TEST_ENABLED 1
+#define ASSERT_ENABLED 0
+#define TRACE_ENABLED 0
+#define TEST_ENABLED 0
 
 void errormsg(char*);
 void die();
