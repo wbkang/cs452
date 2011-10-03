e7c9df10b61c24cfced01e0b96a74e990e504856
pbakhila <pbakhila@localhost>
2011-10-03 13:38:15 -0400

    k2 scaffolding

--- include/syscall.h
+++ include/syscall.h
@@ -2,3 +2,5 @@
 
 #include <util.h>
 #include <assembly.h>
+
+int Send(int tid, char *msg, int msglen, char *reply, int replylen);
--- kernel/syscall.c
+++ kernel/syscall.c
@@ -1,2 +1,10 @@
 #include <syscall.h>
 
+#define MAX_MSG_LEN 0xFFFF
+
+int Send(int tid, char *msg, int msglen, char *reply, int replylen) {
+	ASSERT(msglen > 0 && msglen <= MAX_MSG_LEN, "message length out of bounds");
+	ASSERT(replylen > 0 && replylen <= MAX_MSG_LEN, "reply length out of bounds");
+	uint lengths = (((uint) replylen) << 16) | msglen;
+	return asm_Send(tid, msg, reply, lengths);
+}
