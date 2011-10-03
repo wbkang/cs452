9fec6c18b88b5b4467c6ae31aa12feeca9093580
Woongbin Kang <wbk@outlook.com>
2011-10-03 11:00:09 -0400

    improving the build script

--- script/build_remote.sh
+++ script/build_remote.sh
@@ -1,37 +1,62 @@
 #!/bin/sh
 
-CURCS=`hg log | head -1 | awk '{ print $2 }' | cut -d ":" -f 2`
-PRIVATEKEY=/c/users/wbkang/.ssh/privatekey
+if which hg; then
+	HG=hg
+elif which /usr/bin/hg; then
+	HG=/usr/bin/hg
+else 
+	echo "Mercurial not found!"
+	exit 1
+fi
 
-hg commit -m "temp"
+CURCS=`$HG log | head -1 | awk '{ print $2 }' | cut -d ":" -f 2`
+echo "Current changeset: $CURCS"
+
+REMOTEUSER=$1
+REMOTEHOST=linux024.student.cs.uwaterloo.ca
+PRIVATEKEY=$2
+
+if [ ! -z "$PRIVATEKEY" ]; then
+	echo "Using the specified private key $PRIVATEKEY"
+	PKOPT="-i $PRIVATEKEY"
+fi
+
+echo "***Do not edit***"
+$HG commit -m "temp"
 
 if [ $? -ne 0 ]; then
 	echo "Just running remote build only"
-	ssh -i $PRIVATEKEY wbkang@linux024.student.cs.uwaterloo.ca  \
+	ssh $PKOPT $REMOTEUSER@$REMOTEHOST \
 	"
-source cs452.sh;
-cd cs452/kernel;
-hg update;
-./clean.sh;
-./build.sh;
-"	
-	exit 1;
+		[ -f cs452.sh ] && source cs452.sh;
+		cd cs452/kernel;
+		hg update;
+		./clean.sh;
+		./build.sh;
+	"	
+	exit 0;
 fi
 
-TEMPCS=`hg log | head -1 | awk '{ print $2 }' | cut -d ":" -f 2`
+TEMPCS=`$HG log | head -1 | awk '{ print $2 }' | cut -d ":" -f 2`
+echo "The new temporary changeset: $TEMPCS"
 
-hg push -f -r $TEMPCS 
+$HG update -C $CURCS
+$HG revert -a -r $TEMPCS
+echo "***You may start editing your files again***"
 
-ssh -i $PRIVATEKEY wbkang@linux024.student.cs.uwaterloo.ca \
+$HG push -f -r $TEMPCS 
+$HG strip -f $TEMPCS
+
+echo "***Starting a remote build...***"
+ssh $PKOPT $REMOTEUSER@$REMOTEHOST \
 "
-source cs452.sh;
-cd cs452/kernel;
-hg update -C $TEMPCS  > /dev/null;
-./clean.sh;
-./build.sh;
-hg update `hg parent | grep changeset: | awk '{print $2}' | cut -d ':' -f 1`
-hg strip $TEMPCS > /dev/null;"
+	[ -f cs452.sh ] && source cs452.sh;
+	cd cs452/kernel;
+	hg update -C $TEMPCS  > /dev/null;
+	./clean.sh;
+	./build.sh;
+	hg update `hg parent | grep changeset: | awk '{print $2}' | cut -d ':' -f 1`
+	hg strip $TEMPCS > /dev/null;
+"
 
-hg update $CURCS
-hg revert -a -r $TEMPCS
-hg strip $TEMPCS
+
