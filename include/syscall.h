1a4b7b05e55e5c0a81c79ba6d372b2df6b391e0f
pbakhila <1944571+pavel-bakhilau@users.noreply.github.com>
2011-10-03 11:04:15 -0400

    refactoring + asm syscalls

--- include/syscall.h
+++ include/syscall.h
@@ -1,15 +1,4 @@
 #pragma once
 
 #include <util.h>
-
-int Create(int priority, func_t code);
-
-int MyTid();
-
-int MyParentsTid();
-
-void Pass();
-
-void Exit();
-
-void* malloc(unsigned int size);
+#include <assembly.h>
