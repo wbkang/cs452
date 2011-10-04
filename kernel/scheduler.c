0ad04be7bd6b34b879cfcabcdda93d3472962f00
pbakhila <1944571+pavel-bakhilau@users.noreply.github.com>
2011-10-03 20:00:12 -0400

    cleaner message passing (untested)

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -3,23 +3,22 @@
 #include <memory.h>
 
 static task_descriptor *running;
+static task_descriptor waiting4send;
+static task_descriptor waiting4reply;
 static priorityq *ready_queue;
 static int reschedule;
 
 void scheduler_init() {
 	running = NULL;
 	reschedule = TRUE;
+	TD_CLOSE(&waiting4send);
+	TD_CLOSE(&waiting4reply);
 	TRACE("\tready queue of size %d initialized", TASK_LIST_SIZE);
 	ready_queue = priorityq_new(TASK_LIST_SIZE, NUM_PRIORITY);
 }
 
 task_descriptor *scheduler_running() {
 	return running;
-}
-
-void scheduler_ready(task_descriptor *td) {
-	td->state = TD_STATE_READY;
-	priorityq_push(ready_queue, td, td->priority);
 }
 
 int scheduler_empty() {
@@ -49,4 +48,28 @@ void scheduler_move2ready() {
 
 void scheduler_runmenext() {
 	reschedule = FALSE;
+}
+
+void scheduler_ready(task_descriptor *td) {
+	TD_REMOVE(td);
+	td->state = TD_STATE_READY;
+	priorityq_push(ready_queue, td, td->priority);
+}
+
+void scheduler_wait4send(task_descriptor *reciever) {
+	TD_REMOVE(reciever);
+	reciever->state = TD_STATE_WAITING4SEND;
+	TD_PUSH(&waiting4send, reciever);
+}
+
+void scheduler_wait4recieve(task_descriptor *source, task_descriptor *td) {
+	TD_REMOVE(td);
+	td->state = TD_STATE_WAITING4RECIEVE;
+	TD_PUSH(source, td);
+}
+
+void scheduler_wait4reply(task_descriptor *sender) {
+	TD_REMOVE(sender);
+	sender->state = TD_STATE_WAITING4REPLY;
+	TD_PUSH(&waiting4reply, sender);
 }
