813f9ef4d584e488c56370cb2c83a2a03c96a5f2
wbkang <wbkang@localhost>
2011-10-06 23:45:23 -0400

    Propser use of volatile memptrs in util.h and rawio.c.

--- main/main.c
+++ main/main.c
@@ -6,6 +6,15 @@
 
 int main(int argc, char *argv[]) {
 	raw_init();
+#ifndef __X86
+//	__asm volatile (
+//			"mrc p15, 0, r0, c1, c0, 0 \n\t"
+//			"ldr r1, =0x1005\n\t"
+//			"orr r0, r0, r1\n\t"
+//			"mcr p15, 0, r0, c1, c0, 0 \n\t"
+//			: : : "r0", "r1");
+#endif
+	TRACE("MAIN START");
 	test_run();
 	kerneltest_run();
 	kernel_init();
