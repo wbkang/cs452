d84a7b515cd8cf01c503a9900c2d791593d580b3
Woongbin Kang <wbk@outlook.com>
2011-10-06 23:41:19 -0400

    memcmp

--- include/string.h
+++ include/string.h
@@ -17,3 +17,5 @@ void strcpy(char *dest, const char *src);
 uint strparseuint(char *str, int *idx);
 
 void* memcpy(void* destination, const void* source, uint num);
+
+int memcmp(const void *p1, const void *p2, uint count);
