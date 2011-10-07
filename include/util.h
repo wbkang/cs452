e703497e0b3a1a9f4f2aa4ebfdfcca3a4ad7b290
Woongbin Kang <wbk@outlook.com>
2011-10-06 23:45:23 -0400

    Propser use of volatile memptrs in util.h and rawio.c.

--- include/util.h
+++ include/util.h
@@ -7,6 +7,7 @@
 
 typedef unsigned int uint;
 typedef uint *memptr;
+typedef uint volatile * volatile vmemptr;
 typedef void (*func_t)();
 
 ////////// VAR ARG
@@ -23,8 +24,7 @@ typedef char *va_list;
 		         (((ap) = (ap) + __va_argsiz(t)), *((t*) (void*) ((ap) - __va_argsiz(t))))
 
 ///////////// USEFUL MACROS
-#define MEM(x) (*(memptr)(x))
-#define VMEM(x) (*(volatile memptr)(x))
+#define VMEM(x) (*(vmemptr)(x))
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 #define MAX(x, y) ( ( (x) > (y) ) ? (x) : (y) )
@@ -36,7 +36,7 @@ typedef char *va_list;
 #define NEXTHIGHESTWORD(x) BYTES2WORDS((x) + 3)
 // 2k clock, divide by 2 to get time in ms, with 6 ticks per 2000 ticks drift correction
 #define GET_TIME(time) { \
-    time = WORD ^ RAM(TIMER3_BASE + VAL_OFFSET); \
+    time = ~VMEM(TIMER3_BASE + VAL_OFFSET); \
     time += (3 * time) / 1000; \
     time >>= 1; \
 }
