fb31457e991ba8f80bf8d6cde1ab099dc4f2ca5f
pbakhila <pbakhila@localhost>
2011-10-08 15:12:37 -0400

    improved heap + min heap (untested)

--- kernel/heap.c
+++ kernel/heap.c
@@ -1,18 +1,6 @@
 #include <heap.h>
 #include <memory.h>
 
-#define HEAP_NODEMAG(n) ((n).key)
-#define HEAP_GT(h, i, j) (HEAP_NODEMAG(h->arr[i]) > HEAP_NODEMAG(h->arr[j]))
-#define HEAP_GE(h, i, j) (HEAP_NODEMAG(h->arr[i]) >= HEAP_NODEMAG(h->arr[j]))
-#define HEAP_PARENT(i) ((i) >> 1) // floor(i / 2)
-#define HEAP_LEFTCHILD(i) ((i) << 1) // 2 * i
-#define HEAP_RIGHTCHILD(i) (((i) << 1) | 1) // 2 * i + 1
-#define HEAP_EXCHANGE(h, i, j) { \
-	heap_item tmp = h->arr[i]; \
-	h->arr[i] = h->arr[j]; \
-	h->arr[j] = tmp; \
-}
-
 heap *heap_new(int size) {
 	heap *h = qmalloc(sizeof(heap) + sizeof(heap_item) * size);
 	h->size = 0;
@@ -20,63 +8,128 @@ heap *heap_new(int size) {
 	return h;
 }
 
-void heap_max_heapify(heap *h, uint i) {
-	uint left = HEAP_LEFTCHILD(i);
-	uint right = HEAP_RIGHTCHILD(i);
-	uint largest;
-	if (left < h->size && HEAP_GT(h, left, i)) {
-		largest = left;
-	} else {
-		largest = i;
-	}
-	if (right < h->size && HEAP_GT(h, right, largest)) {
-		largest = right;
-	}
-	if (largest != i) {
-		HEAP_EXCHANGE(h, largest, i);
-		heap_max_heapify(h, largest);
+inline heap_item *heap_peek(heap *h) {
+	return h->size == 0 ? NULL : h->arr;
+}
+
+inline uint heap_parent(uint i) {
+	return i >> 1; // i / 2
+}
+
+inline uint heap_leftchild(uint i) {
+	return i << 1; // i * 2
+}
+
+inline uint heap_rightchild(uint i) {
+	return (i << 1) | 1; // i * 2 + 1
+}
+
+inline uint heap_swap(heap *h, uint i, uint j) {
+	heap_item tmp = h->arr[i];
+	h->arr[i] = h->arr[j];
+	h->arr[j] = tmp;
+	return j;
+}
+
+inline int heap_put(heap *h, void *data, int key) {
+	if (h->size >= h->max_size) return -1;
+	uint i = h->size;
+	h->arr[i].data = data;
+	h->arr[i].key = key;
+	h->size++;
+	return i;
+}
+
+inline void heap_bubbleup_min(heap *h, uint i) {
+	while (i > 0) {
+		uint p = heap_parent(i);
+		if (h->arr[p].key <= h->arr[i].key) return;
+		i = heap_swap(h, i, p);
 	}
 }
 
-void heap_max_heapify2(heap *h, uint i) {
-	uint left, right, largest;
+inline void heap_bubbleup_max(heap *h, uint i) {
+	while (i > 0) {
+		uint p = heap_parent(i);
+		if (h->arr[p].key >= h->arr[i].key) return;
+		i = heap_swap(h, i, p);
+	}
+}
+
+int heap_insert_min(heap *h, void* data, int key) {
+	int i = heap_put(h, data, key);
+	if (i < 0) return i;
+	heap_bubbleup_min(h, i);
+	return 0;
+}
+
+int heap_insert_max(heap *h, void* data, int key) {
+	int i = heap_put(h, data, key);
+	if (i < 0) return i;
+	heap_bubbleup_max(h, i);
+	return 0;
+}
+
+void heap_heapify_min(heap *h, uint i) {
 	for (;;) {
-		left = HEAP_LEFTCHILD(i);
-		if (left >= h->size) return;
-		right = HEAP_RIGHTCHILD(i);
-		if (right < h->size && HEAP_GT(h, right, i)) {
-			largest = HEAP_GT(h, left, right) ? left : right;
+		uint left = heap_leftchild(i);
+		if (left >= h->size) return; // no left child => no right child
+		uint right = heap_rightchild(i);
+		if (right < h->size && h->arr[right].key < h->arr[i].key) {
+			if (h->arr[left].key < h->arr[right].key) {
+				i = heap_swap(h, i, left);
+			} else {
+				i = heap_swap(h, i, right);
+			}
 		} else {
-			if (HEAP_GT(h, left, i)) {
-				largest = left;
+			if (h->arr[left].key < h->arr[i].key) {
+				i = heap_swap(h, i, left);
 			} else {
 				return;
 			}
 		}
-		HEAP_EXCHANGE(h, largest, i);
-		i = largest;
 	}
 }
 
-void* heap_extract_max(heap *h) {
-	ASSERT(h->size != 0, "popping an empty heap");
-	void* rv = h->arr[0].data;
+void heap_heapify_max(heap *h, uint i) {
+	for (;;) {
+		uint left = heap_leftchild(i);
+		if (left >= h->size) return; // no left child => no right child
+		uint right = heap_rightchild(i);
+		if (right < h->size && h->arr[right].key > h->arr[i].key) {
+			if (h->arr[left].key > h->arr[right].key) {
+				i = heap_swap(h, i, left);
+			} else {
+				i = heap_swap(h, i, right);
+			}
+		} else {
+			if (h->arr[left].key > h->arr[i].key) {
+				i = heap_swap(h, i, left);
+			} else {
+				return;
+			}
+		}
+	}
+}
+
+void* heap_extract_min(heap *h) {
+	heap_item *top = heap_peek(h);
+	if (top == NULL) return NULL;
 	h->size--;
-	h->arr[0] = h->arr[h->size];
-	heap_max_heapify(h, 0);
+	void* rv = top->data;
+	if (h->size == 0) return rv;
+	*top = h->arr[h->size];
+	heap_heapify_min(h, 0);
 	return rv;
 }
 
-void heap_max_insert(heap *h, void* data, int key) {
-	ASSERT(h->size != h->max_size, "inserting into a full heap");
-	uint i = h->size, parent;
-	h->arr[i].data = data;
-	h->arr[i].key = key;
-	h->size++;
-	while (i > 0) {
-		parent = HEAP_PARENT(i);
-		if (HEAP_GE(h, parent, i)) return;
-		HEAP_EXCHANGE(h, i, parent);
-		i = parent;
-	}
+void* heap_extract_max(heap *h) {
+	heap_item *top = heap_peek(h);
+	if (top == NULL) return NULL;
+	h->size--;
+	void* rv = top->data;
+	if (h->size == 0) return rv;
+	*top = h->arr[h->size];
+	heap_heapify_max(h, 0);
+	return rv;
 }
