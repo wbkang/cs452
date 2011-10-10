3848a8e2f4c95c8a1daf54ede1f9c219e865a5b1
Woongbin Kang <wbk@outlook.com>
2011-10-10 01:23:51 -0400

    Cleaned up priority q code.

--- kernel/priorityq.c
+++ kernel/priorityq.c
@@ -1,14 +1,5 @@
 #include <priorityq.h>
 #include <memory.h>
-inline static nlz(register unsigned int x);
-
-#define LT(n) n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n
-
-static const char LogTable256[256] = {
-    -1, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
-    LT(4), LT(5), LT(5), LT(6), LT(6), LT(6), LT(6),
-    LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7)
-};
 
 priorityq *priorityq_new(int size, int num_priorities) {
 	priorityq *pq = qmalloc(sizeof(priorityq) + sizeof(queue*) * num_priorities);
@@ -22,39 +13,6 @@ inline int priorityq_empty(priorityq *pq) {
 	return pq->len == 0;
 }
 
-inline int priorityq_maxp(priorityq *pq) {
-	int p;
-	for (p = pq->num_priorities - 1; queue_empty(pq->q[p]); --p);
-	return p;
-}
-
-int log2pp(unsigned int v) {
-	register unsigned int a, b;
-	if ((a = v >> 16)) {
-		if ((b = a >> 8)) {
-			return 24 + LogTable256[b];
-		} else {
-			return 16 + LogTable256[a];
-		}
-	} else {
-		if ((b = v >> 8)) {
-			return 8 + LogTable256[b];
-		} else {
-			return LogTable256[v];
-		}
-	}
-}
-
-inline int log2(register unsigned int v) {
-	register unsigned int i = 0;
-	if (v & 0xffff0000) { v >>= 16; i |= 16; }
-	if (v & 0xff00)     { v >>= 8;  i |= 8;  }
-	if (v & 0xf0)       { v >>= 4;  i |= 4;  }
-	if (v & 0xc)        { v >>= 2;  i |= 2;  }
-	if (v & 0x2)        { v >>= 1;  i |= 1;  }
-	return i;
-}
-
 inline int log2p(register unsigned int v) {
 	register unsigned int l, i = 0;
 	if ((l = v >> 16)) { v = l; i |= 16; }
@@ -65,50 +23,8 @@ inline int log2p(register unsigned int v) {
 	return i;
 }
 
-inline int log2ppp(register unsigned int v) {
-	register unsigned int r; // result of log2(v) will go here
-	register unsigned int shift;
-
-	r =     (v > 0xFFFF) << 4; v >>= r;
-	shift = (v > 0xFF  ) << 3; v >>= shift; r |= shift;
-	shift = (v > 0xF   ) << 2; v >>= shift; r |= shift;
-	shift = (v > 0x3   ) << 1; v >>= shift; r |= shift;
-	                                        r |= (v >> 1);
-	return r;
-}
-
-inline int priorityq_maxpp(priorityq *pq) {
-	return log2ppp(pq->len);
-}
-
-inline int priorityq_maxppp(priorityq *pq) {
-	unsigned int v = pq->len;  // 32-bit value to find the log2 of
-	const unsigned int b[] = {0x2, 0xC, 0xF0, 0xFF00, 0xFFFF0000};
-	const unsigned int S[] = {1, 2, 4, 8, 16};
-	register unsigned int r = 0;
-	for (int i = 4; i >= 0; i--) {
-		if (v & b[i]) {
-			v >>= S[i];
-			r |= S[i];
-		}
-	}
-	return r;
-}
-
-//inline void* priorityq_pop(priorityq *pq) {
-//	ASSERT(!priorityq_empty(pq), "popping an empty priority queue");
-//	pq->len--;
-//	return queue_pop(pq->q[priorityq_maxp(pq)]);
-//}
-//
-//inline void priorityq_push(priorityq *pq, void* item, uint priority) {
-//	ASSERT(priority < pq->num_priorities, "priority too high (%x)", priority);
-//	pq->len++;
-//	queue_push(pq->q[priority], item);
-//}
-
 inline void* priorityq_pop(priorityq *pq) {
-	uint p = priorityq_maxp(pq);
+	uint p = log2p(pq->len);
 	queue *q = pq->q[p];
 	ASSERT(!queue_empty(q), "queue is actually not empty! priority: %d, len: %x", p, pq->len);
 	void *rv = queue_pop(q);
@@ -121,31 +37,3 @@ inline void priorityq_push(priorityq *pq, void* item, uint priority) {
 	queue_push(pq->q[priority], item);
 }
 
-// from hacker's delight
-inline static nlz(register unsigned int x) {
-	register int y, m, n;
-
-	y = -(x >> 16);
-	m = (y >> 16) & 16;
-	n = 16 - m;
-	x = x >> m;
-
-	y = x - 0x100;
-	m = (y >> 16) & 8;
-	n = n + m;
-	x = x << m;
-
-	y = x - 0x1000;
-	m = (y >> 16) & 4;
-	n = n + m;
-	x = x << m;
-
-	y = x - 0x4000;
-	m = (y >> 16) & 2;
-	n = n + m;
-	x = x << m;
-
-	y = x >> 14;
-	m = y & ~(y >> 1);
-	return n + 2 - m;
-}
