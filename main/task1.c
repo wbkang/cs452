a7749b23ef4c9313446595da2b86039782af9936
Woongbin Kang <wbk@outlook.com>
2011-10-09 23:29:10 -0400

    Perfmon changes.

--- main/task1.c
+++ main/task1.c
@@ -4,7 +4,16 @@
 #include <rpsclient.h>
 #include <perfmon.h>
 
+#define RPS_SERVER 0
+#define PERFMON 1
+
 void task1() {
+#if RPS_SERVER
 	Create(1, rps_server);
 	for (int i = 0; i < 4; i++) Create(1, rps_client);
+#endif
+
+#if PERFMON
+	Create(1, perfmon);
+#endif
 }
