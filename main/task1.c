5e5e1a03fc7f7db02633cc6c5c58cc30f88e826a
wbkang <wbkang@localhost>
2011-11-02 15:52:48 -0400

    Semi-fixed the menu.

--- main/task1.c
+++ main/task1.c
@@ -5,40 +5,41 @@
 #include <k3clients.h>
 #include <ioserver.h>
 #include <a0.h>
-
-#define RPS_SERVER 0
-#define PERFMON 0
-#define K3 0
-#define A0 1
+#include <stdio.h>
 
 void task1() {
 	Create(PRIORITY_TIMESERVER, timeserver);
 	ioserver_create(COM1, OFF, 2400, 2, 8, OFF);
 	int com2svr = ioserver_create(COM2, OFF, 115200, 1, 8, OFF);
 
-	char *menu =
-			"Menu:\n"
-			"0. A0\n"
-			"1. Send-Receive-Reply (receiver first, 64 bytes)\n"
-			"2. Kernel 3\n";
+	struct {
+		uint priority;
+		func_t prog;
+		char name[256];
+	} programs[] = {
+			{ 1, a0, "A0" },
+			{ 1, perfmon, "Send-Receive-Reply Performance Test (receiver first, 64 bytes)" },
+			{ 10, k3main, "Kernel 3 ???" }
+	};
 
-	Putstr(COM2, menu, com2svr);
+	int program_count = sizeof(programs) / sizeof(programs[0]);
+	char menubuf[(sizeof(programs[0].name) + 4) * program_count], *b = menubuf;
 
-	char c;
+	for (int i = 0; i < program_count; i++) {
+		b += sprintf(b, "%d.\t%s\n", i, programs[i].name);
+	}
+
 	while (TRUE) {
+		Putstr(COM2, menubuf, com2svr);
 		char c = Getc(COM2, com2svr);
-		switch (c) {
-			case '0':
-				Create(1, a0);
-				break;
-			case '1':
-				Create(1, perfmon);
-				break;
-			case '2':
-				Create(10, k3main);
-				break;
-			default:
-				continue;
+		int idx = c - '0';
+		if (idx < program_count) {
+			Putstr(COM2, "You have selected: ", com2svr);
+			Putc(COM2, c, com2svr);
+			Putc(COM2, '\n', com2svr);
+			Flush(com2svr);
+			Create(programs[idx].priority, programs[idx].prog);
+			return;
 		}
 	}
 }
