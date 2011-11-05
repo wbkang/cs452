14f29c199b349e8dd0711086be4195a17419bfff
pbakhila <pbakhila@localhost>
2011-11-05 17:30:27 -0400

    fixed point fix

--- include/fixed.h
+++ include/fixed.h
@@ -1,30 +1,51 @@
-#define FIXED_Q 16
+#include <util.h>
+#include <stdio.h>
+
+#define FIXED_Q 14 // corresponds to 4 correct decimal digits
 #define FIXED_K (1 << (FIXED_Q - 1))
-#define FIXED_A (FIXED_Q * 10) / 33 // accuracy
-#define FIXED_10toA 10000
+#define FIXED_10toA 10000 // 10^floor(log10(2^Q - 1)) -- num frac digits to print
+
+#define FIXED_MAX_N ((1 << (32 - 1 - FIXED_Q)) - 1)
+#define FIXED_MIN_N (-1 << (32 - 1 - FIXED_Q))
 
 typedef int fixed;
 
-static inline int fixed_new(int inte, int frac) {
-	ASSERT(frac >= 0, "bad fractional part");
-	ASSERT(frac < (1 << FIXED_Q), "bad fractional part");
-	ASSERT(abs(inte) < (1 << (32 - 1 - FIXED_Q)), "bad integer part");
-	return (inte << FIXED_Q) | frac;
+static inline int fixed_new(int n) {
+	ASSERT(n >= FIXED_MIN_N, "n too small");
+	ASSERT(n <= FIXED_MAX_N, "n too big");
+	return n << FIXED_Q;
 }
 
 static inline int fixed_int(fixed n) {
+	if (n < 0) {
+		n = -n;
+	}
 	return n >> FIXED_Q;
 }
 
-static inline int fixed_fra(char *buf, fixed n) {
-	long long f = n & ((1 << FIXED_Q) - 1);
-	long long fra = (f * FIXED_10toA) >> FIXED_Q;
-	PRINT("(%d)", fra);
-	for (int i = 0, q = FIXED_10toA; i < FIXED_A; i++, q /= 10) {
-		int d = (fra / q) % 10;
-		buf[i] = '0' + d;
+static inline int fixed_fra(fixed n) {
+	if (n < 0) {
+		n = -n;
 	}
-	return FIXED_A;
+	return n & ((1 << FIXED_Q) - 1);
+}
+
+static inline int fixed_print(char *buf, fixed n) {
+    char *b = buf;
+	if (n < 0) {
+		*b++ = '-';
+	}
+	b += sprintf(b, "%d", fixed_int(n));
+	*b++ = '.';
+	int fra = ((long long) fixed_fra(n) * FIXED_10toA) >> FIXED_Q;
+	int q;
+	for (q = FIXED_10toA / 10; q; q /= 10) {
+		int d = fra / q;
+		fra -= d * q;
+		*b++ = '0' + d;
+	}
+	*b = '\0';
+	return b - buf;
 }
 
 static inline fixed fixed_add(fixed a, fixed b) {
@@ -37,15 +58,17 @@ static inline fixed fixed_sub(fixed a, fixed b) {
 	return a - b;
 }
 
+// @TODO: add overflow assert
 static inline fixed fixed_mul(fixed a, fixed b) {
 	long long tmp = (long long) a * (long long) b;
 	tmp += FIXED_K; // round
 	return tmp >> FIXED_Q;
 }
 
+// @TODO: add overflow assert
 static inline fixed fixed_div(fixed a, fixed b) {
-	ASSERT(b, "div by zero!");
+	ASSERT(b != 0, "div by zero");
 	long long tmp = ((long long) a) << FIXED_Q;
-	tmp += b >> 1;
+	tmp += b >> 1; // round
 	return tmp / b;
 }
