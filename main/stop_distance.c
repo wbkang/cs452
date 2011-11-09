df3aaf975eca42dfd059e003488a8bf830bd1759
wbkang <wbkang@localhost>
2011-11-09 08:50:14 -0500

    stop distance update

--- main/stop_distance.c
+++ main/stop_distance.c
@@ -3,8 +3,8 @@
 void populate_stop_distance(train_descriptor *train, int trainnum) {
 	switch (trainnum) {
 		case 38:
-			train->stopm = fixed_div(fixed_new(53911), fixed_new(1000));
-			train->stopb = fixed_div(fixed_new(-60000), fixed_new(1000));
+			train->stopm = fixed_div(fixed_new(52612), fixed_new(1000));
+			train->stopb = fixed_div(fixed_new(-36780), fixed_new(1000));
 			break;
 	}
 }
