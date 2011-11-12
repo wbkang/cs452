c2165589454b56d76b44309c6a41313a3755a6b2
pbakhila <1944571+pavel-bakhilau@users.noreply.github.com>
2011-11-12 13:39:26 -0500

    improved location object (untested)

--- main/location.c
+++ main/location.c
@@ -1,36 +1,55 @@
 #include <location.h>
 
 void location_init(location *this, track_edge *edge, fixed offset) {
+	ASSERTNOTNULL(this);
 	this->edge = edge;
-	this->offset = offset;
+	this->offset = fixed_new(0);
+	location_inc(this, offset);
 }
 
 void location_init_undef(location *this) {
+	ASSERTNOTNULL(this);
 	this->edge = NULL;
 }
 
 int location_isundef(location *this) {
-	return !this || this->edge == NULL;
+	ASSERTNOTNULL(this);
+	return this->edge == NULL;
+}
+
+int location_isvalid(location *this) {
+	if (this == NULL) return FALSE;
+	if (this->edge == NULL) return TRUE; // undefined
+	if (fixed_sgn(this->offset) < 0) return FALSE; // negative offset
+	fixed edge_len = fixed_new(this->edge->dist);
+	if (fixed_cmp(this->offset, edge_len) >= 0) return FALSE; // "overflow" offset
+	return TRUE;
 }
 
 fixed location_dist(location *from, location *to) {
-	int dist = track_distance(from->edge->src, to->edge->src);
-	if (dist < 0) {
-		fixed rv = {-1};
-		return rv;
+	ASSERT(location_isvalid(from), "bad 'from' location");
+	ASSERT(location_isvalid(to), "bad 'to' location");
+	if (!location_isundef(from) && !location_isundef(to)) {
+		int dist = track_distance(from->edge->src, to->edge->src);
+		if (dist >= 0) {
+			return fixed_sub(fixed_add(fixed_new(dist), from->offset), to->offset);
+		}
 	}
-	return fixed_sub(fixed_add(fixed_new(dist), from->offset), to->offset);
+	fixed rv = {-1};
+	return rv;
 }
 
+// @TODO: problem here if slightly over-increment past an exit/enter
+// @TODO: uses current switch state. return multiple 'virtual' locations instead?
 void location_inc(location *this, fixed dx) {
-	if (!location_isundef(this)) {
-		this->offset = fixed_add(this->offset, dx);
-		do {
-			fixed fdist = fixed_new(this->edge->dist);
-			if (fixed_cmp(this->offset, fdist) < 0) return;
-			this->offset = fixed_sub(this->offset, fdist);
-			this->edge = track_next_edge(this->edge->dest);
-		} while (this->edge);
-	}
+	ASSERT(location_isvalid(this), "bad location");
+	ASSERT(!location_isundef(this), "undefined locations");
+	this->offset = fixed_add(this->offset, dx);
+	do {
+		fixed edge_len = fixed_new(this->edge->dist);
+		if (fixed_cmp(this->offset, edge_len) < 0) return;
+		this->offset = fixed_sub(this->offset, edge_len);
+		this->edge = track_next_edge(this->edge->dest);
+	} while (this->edge);
 	location_init_undef(this);
 }
