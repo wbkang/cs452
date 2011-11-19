da20df8875fff3ca9db4d01ca6503326235536bd
Woongbin Kang <wbk@outlook.com>
2011-11-18 20:15:26 -0500

    fixed the busted strncpy

--- main/ui/logdisplay.c
+++ main/ui/logdisplay.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <console.h>
 
+#define ROUND_ROBIN_PREFIX "> "
+
 logdisplay *logdisplay_new(console *con, int line, int col, int totallines, int totalcols, int rotation) {
 	logdisplay *this = malloc(sizeof(logdisplay) + sizeof(char*) * totallines);
 	this->con = con;
@@ -11,6 +13,7 @@ logdisplay *logdisplay_new(console *con, int line, int col, int totallines, int 
 	this->col = col;
 	this->totallines = totallines;
 	this->totalcols = totalcols;
+	if (rotation == ROUNDROBIN) this->totalcols -= strlen(ROUND_ROBIN_PREFIX); // accounting for the starting arrow
 	this->curcol = 0;
 	this->curline = 0;
 	this->topline = 0;
@@ -39,21 +42,22 @@ void logdisplay_puts(logdisplay *this, char *str) {
 
 void logdisplay_flushline(logdisplay *this) {
 	this->curcol = 0;
+	char fmtbuf[10];
 	if (this->rotation == ROUNDROBIN) {
 		int last_line = (this->totallines + this->curline - 1) % this->totallines;
 		console_move(this->con, this->line + last_line, this->col);
 		console_printf(this->con, " ", this->buf[last_line]);
 		console_move(this->con, this->line + this->curline, this->col);
-		console_erase_eol(this->con);
-		console_printf(this->con, "> %s", this->buf[this->curline]);
+		sprintf(fmtbuf, ROUND_ROBIN_PREFIX "%%-%ds", this->totalcols);
+		console_printf(this->con, fmtbuf, this->buf[this->curline]);
 		console_flush(this->con);
 		this->curline++;
 		this->curline = this->curline % this->totallines;
 	} else if (this->rotation == SCROLLING) {
+		sprintf(fmtbuf, "%%-%ds", this->totalcols);
 		for (int i = 0, cl = this->topline % this->totallines; i < this->totallines; i++) {
 			console_move(this->con, this->line + i, this->col);
-			console_erase_eol(this->con);
-			console_printf(this->con, "%s", this->buf[cl]);
+			console_printf(this->con, fmtbuf, this->buf[cl]);
 			cl++;
 			cl = cl % this->totallines;
 		}
