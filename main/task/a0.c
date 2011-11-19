274ba29e598f630b2aad3fca3b81e3ab1c75ebf6
Woongbin Kang <wbk@outlook.com>
2011-11-18 19:45:44 -0500

    column limited logdisplay

--- main/task/a0.c
+++ main/task/a0.c
@@ -578,7 +578,7 @@ void a0() {
 	state.cmdlog = logstrip_new(state.con, CONSOLE_LOG_LINE, CONSOLE_LOG_COL);
 	state.cmdline = cmdline_new(state.con, CONSOLE_CMD_LINE, CONSOLE_CMD_COL, handle_command, &state);
 	state.sensorlog = logstrip_new(state.con, CONSOLE_SENSOR_LINE, CONSOLE_SENSOR_COL);
-	state.log = logdisplay_new(state.con, CONSOLE_DUMP_LINE, CONSOLE_DUMP_COL, 20, SCROLLING);
+	state.log = logdisplay_new(state.con, CONSOLE_DUMP_LINE, CONSOLE_DUMP_COL, 20, 40, SCROLLING);
 
 	state.trainloc = logstrip_new(state.con, 9, 58);
 
