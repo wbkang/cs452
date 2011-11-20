e30d94b565736f023081a77cd4ecf6bfd1f4f859
pbakhila <pbakhila@localhost>
2011-11-19 23:00:18 -0500

    cleanup

--- main/include/server/buffertask.h
+++ main/include/server/buffertask.h
@@ -1,7 +1,7 @@
 #pragma once
 
+void buffertask();
+
 int buffertask_new(char name[], int priority, int item_size);
 int buffertask_put(int tid, void* item, int item_size);
 int buffertask_get(int tid, void* item, int item_size);
-
-void buffertask();
