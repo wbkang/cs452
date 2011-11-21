039e2178456644d6457bd96ec7511c2da125ae93
pbakhila <1944571+pavel-bakhilau@users.noreply.github.com>
2011-11-21 09:03:09 -0500

    cleanup + remove malloc every dijkstra()...

--- main/include/gps.h
+++ main/include/gps.h
@@ -3,11 +3,14 @@
 #include <engineer.h>
 #include <train.h>
 #include <location.h>
+#include <heap.h>
+#include <track_node.h>
 
 typedef struct gps gps;
 
 struct gps {
 	track_node *track_node;
+	heap *heap_dijkstra;
 };
 
 gps *gps_new(track_node *nodes);
